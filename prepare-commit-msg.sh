1b3cc7618885b87b7479175e39e5bcdbb36818ca
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-13 12:19:03 +0100

    prepare-commit-msg

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -8,4 +8,4 @@ branchPath=$(git symbolic-ref -q HEAD)
 branchName=${branchPath##*/}    #Get text behind the last / of the branch path
 
 firstLine=$(head -n1 $1)
-sed -i '.bak' "1s/^/[$branchName]: " $1   #Insert branch name at the start of the commit message file
+sed -i '.bak' "1s/^/\[$branchName\]: " $1   #Insert branch name at the start of the commit message file
