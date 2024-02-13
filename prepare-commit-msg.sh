f22e1c12b39aa8d4a0eb922ea315f7cbe29bc56b
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-13 12:16:30 +0100

    prepare-commit-msg

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -8,9 +8,4 @@ branchPath=$(git symbolic-ref -q HEAD)
 branchName=${branchPath##*/}    #Get text behind the last / of the branch path
 
 firstLine=$(head -n1 $1)
-echo $firstLine
-sed -i '.bak' "1s/^/$branchName : \n/" $1   #Insert branch name at the start of the commit message file
-
-if [ -z "$firstLine" ] ;then   #Check that this is not an amend by checking that the first line is empty
-    sed -i "1s/^/[$branchName]: \n/" $1   #Insert branch name at the start of the commit message file
-fi
+sed -i '.bak' "1s/^/[$branchName]: " $1   #Insert branch name at the start of the commit message file
