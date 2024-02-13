e85d2f5fe1a6d069333e34cc484764c4bd641958
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-13 10:54:00 +0100

    exit 0

--- post-checkout.sh
+++ post-checkout.sh
@@ -15,15 +15,7 @@ final_out="version NOT changed"
 if [[ "$NUM_CHECKOUTS" == "1" ]]; then
   orig_version=$(grep -E 'repo_version[[:space:]]*=|module_version[[:space:]]*=' locals.tf  | cut -d '"' -f 2)
   new_version=`echo $orig_version| awk -F. -v OFS=. '{$NF += 1 ; print}' 2>/dev/null`
-  if [[ $? -ne 0 ]]; then
-    echo -e "${c_bold}${c_red}FAIL!!\n\t\tERROR:${c_reset} - ${new_version}"
-    return 1
-  fi
   cmd_out=$(sed -i '' -e "s/${orig_version}/${new_version}/" locals.tf 2>&1)
-  if [[ $? -ne 0 ]]; then
-    echo -e "${c_bold}${c_red}FAIL!!\n\t\tERROR:${c_reset} - ${cmd_out}"
-    return 1
-  fi
   final_out="version increased!"
 fi
 
