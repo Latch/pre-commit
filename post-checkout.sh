fe2c1009e8741eee8ba0d14f6db6e32b21cb642e
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-13 11:50:51 +0100

    Add prepare-commit-msg

--- post-checkout.sh
+++ post-checkout.sh
@@ -15,11 +15,19 @@ final_out="version NOT changed"
 if [[ "$NUM_CHECKOUTS" == "1" ]]; then
   orig_version=$(grep -E 'repo_version[[:space:]]*=|module_version[[:space:]]*=' locals.tf  | cut -d '"' -f 2)
   new_version=`echo $orig_version| awk -F. -v OFS=. '{$NF += 1 ; print}' 2>/dev/null`
+  if [[ $? -ne 0 ]]; then
+    echo -e "${c_bold}${c_red}Something went wrong:${c_reset} - ${new_version}"
+    exit 1
+  fi
   cmd_out=$(sed -i '' -e "s/${orig_version}/${new_version}/" locals.tf 2>&1)
-  final_out="version increased!"
+  if [[ $? -ne 0 ]]; then
+    echo -e "${c_bold}${c_red}Something went wrong:${c_reset} - ${cmd_out}"
+    exit 1
+  fi
+  final_out="Version increased!"
 fi
 
-echo -e "${c_bold}${c_green}OK!! ${final_out}${c_reset}"
+echo -e "${c_bold}${c_green}${final_out}${c_reset}"
 echo -e "${c_bold}--- POST-CHECKOUT-HOOK ---${c_reset}"
 
 exit 0
