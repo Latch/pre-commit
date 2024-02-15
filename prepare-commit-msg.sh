adadfcb8eff58b990edd0eed430a216345b042e2
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-15 11:35:05 +0100

    Fix commit msg

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -2,10 +2,9 @@
 # Automatically adds branch name and branch description to every commit message.
 
 echo ""
-echo " Adding branch name to beginning of the commit messassssge... "
+echo " Adding branch name to beginning of the commit message... "
 
 branchPath=$(git symbolic-ref -q HEAD)
 branchName=${branchPath##*/}    #Get text behind the last / of the branch path
 
-firstLine=$(head -n1 $1)
 sed -i '.bak' "1s/^/\[$branchName\]: /" $1   #Insert branch name at the start of the commit message file
