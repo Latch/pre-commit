81701179b1894866d0cf7b45c0f2ad1c0952ae3c
joanpolalejandre <joan.obiols@latch.com>
2024-02-23 12:12:48 +0100

    v1.0.2

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -4,6 +4,6 @@
 echo ""
 echo " Adding branch name to beginning of the commit message... "
 
-branchName=${git rev-parse --abbrev-ref HEAD | cut -d'/' -f1}    #Get branch name before /description
+branchName=$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f1)    #Get branch name before /description
 
 sed -i '.bak' "1s/^/\[$branchName\]: /" $1   #Insert branch name at the start of the commit message file
